9133c85f86c623e6e4d85ef4c9e90d745ebd2ff9
Leandro C. Tomassini <leandroctomassini@gmail.com>
2024-05-12 20:58:24 -0300

    Colores

---
+++ workspace/0-base/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{html,js}" ],
+  theme: {
+    extend: {
+      colors: {
+        'azul-claro': '#243cff',
+        'azul-oscuro': '#0D1B3E',
+      }
+    },
+  },
+  plugins: [],
+}
+
